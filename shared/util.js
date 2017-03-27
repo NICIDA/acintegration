0bf76a24cecddc7a1e18e95ece0ec38922cdc115
NICIDA <FABIO.NICIDA@AGILA.COM.BR>
2017-03-27 09:56:07 -0300

    Função remove removida - US71

--- shared/util.js
+++ shared/util.js
@@ -1,5 +1,5 @@
 /* @flow */
-
+/**
 /**
  * Convert a value to a string that is actually rendered.
  */
@@ -43,17 +43,7 @@ export function makeMap (
  */
 export const isBuiltInTag = makeMap('slot,component', true)
 
-/**
- * Remove an item from an array
- */
-export function remove (arr: Array<any>, item: any): Array<any> | void {
-  if (arr.length) {
-    const index = arr.indexOf(item)
-    if (index > -1) {
-      return arr.splice(index, 1)
-    }
-  }
-}
+
 
 /**
  * Check whether the object has the property.
