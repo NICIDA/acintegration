404c76cc5462a979e0084fc11c3f0e7c11fff6b6
NICIDA <FABIO.NICIDA@AGILA.COM.BR>
2017-03-27 10:00:00 -0300

    Nova implementação de remove - US71

--- shared/util.js
+++ shared/util.js
@@ -43,7 +43,17 @@ export function makeMap (
  */
 export const isBuiltInTag = makeMap('slot,component', true)
 
-
+/**
+ * Remove an item from an array
+ */
+export function remove (arr: Array<any>, item: any): Array<any> | void {
+  if (arr.length) {
+    const index = arr.indexOf(item)
+    if (index > -1) {
+      return arr.splice(index, 1)
+    }
+  }
+}
 
 /**
  * Check whether the object has the property.
